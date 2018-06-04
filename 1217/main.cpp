19b95a4e410fa25f5fc0da83e9efe98fd961dadf
Hunterhuan <879090429@qq.com>
2018-06-04 14:54:11 +0800

    trash

---
+++ 1217/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int num[2505];
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>num[i];
+    }
+    int m;
+    cin>>m;
+    int tmp;
+    int j;
+    for(int i=0;i<m;++i){
+        cin>>tmp;
+        for(j=0;j<n;++j){
+            if(tmp==num[j])
+            {
+                cout<<"Y"<<endl;
+                break;
+            }
+        }
+        if(j==n)
+            cout<<"N"<<endl;
+    }
+    cout << "Hello world!" << endl;
+    return 0;
+}
